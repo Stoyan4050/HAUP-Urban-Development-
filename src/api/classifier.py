015e2b514ed12079a000880cb7866a942283c2da
Stoyan <s.g.dimitrov@student.tudelft.nl>
2021-05-20 00:20:41 +0300

    fixing bugs

--- src/api/classifier.py
+++ src/api/classifier.py
@@ -13,6 +13,7 @@ from sklearn.pipeline import Pipeline
 from sklearn.pipeline import make_pipeline
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
+from django.db.models import Q
 
 from .models import Classification, Tile
 def getImageFromURL(year, x_coord, y_coord):
@@ -23,42 +24,68 @@ def getImageFromURL(year, x_coord, y_coord):
     img = cv2.resize(cv2.imread(res[0],1), (32,32))
     return img
 
-def getImages(year=2020):
-    data = Classification.objects.all()
+def getImagesTraining(data, year):
     training_imgs = []
     counter = 0
     for c in data:
-        print(c.year)
-        tileYear = c.year
-        print(tileYear)
-        if tileYear == "Unknown":
-            tileYear = 2020
+        #print(c.year)
 
-        if tileYear == year:
+        # if tileYear == year:
 
-            tile = c.tile_id
-            print(year)
-            img = getImageFromURL(year, tile.y_coordinate, tile.x_coordinate)
-            print(counter)
-            training_imgs.append((img, c.label))
-            counter+=1
+        tile = c.tile_id
+            #print(year)
+        img = getImageFromURL(year, tile.y_coordinate, tile.x_coordinate)
+            #print(counter)
+        training_imgs.append((img, c.label))
+        #print(counter)
+        print(counter)
+        counter+=1
+        # if counter > 100:
+        #     print(len(training_imgs), "IM")
+        #     return training_imgs
 
     return training_imgs
 #
+def getImagesTest(year):
+    Classification.objects.filter(classified_by=-1).delete()
+    data = Classification.objects.filter(~Q(classified_by=-1))
+    #print(data)
+    # data = Tile.objects.exclude(tid=classified)
+    # print(data)
+    test_imgs = []
+    counter = 0
+    for c in data:
+        # print(c.year)
+
+        # if tileYear == year:
+
+        tile = c.tile_id
+        # print(year)
+        img = getImageFromURL(year, tile.y_coordinate, tile.x_coordinate)
+        # print(counter)
+        coord = (tile.y_coordinate, tile.x_coordinate)
+        test_imgs.append((img, coord))
+        print(counter)
+        counter += 1
+
+    return np.array(test_imgs)
+
+
 def random_sample(arr):
     arr = np.array(arr)
-    print(arr)
-    return arr[np.random.choice(len(arr), size=int(len(arr)/10), replace=False)]
+    #print(arr)
+    res = arr[np.random.choice(len(arr), size=int(len(arr)/2), replace=False)]
+    return res
 
-def getPortionOfTheData(data):
-    print(len(data))
-    #np.array(X_train)[indices.astype(int)]
-    ix_size = int(0.05 * len(data))
-    ix = np.random.choice(len(data), size=ix_size, replace=False)
-    train_data_10per = data[ix]
-    np.random.shuffle(train_data_10per)
-    print(len(train_data_10per))
-    return train_data_10per
+# def getPortionOfTheData(data):
+#     print(len(data))
+#     #np.array(X_train)[indices.astype(int)]
+#     ix_size = int(0.05 * len(data))
+#     ix = np.random.choice(len(data), size=ix_size, replace=False)
+#     train_data_10per = data[ix]
+#     np.random.shuffle(train_data_10per)
+#     print(len(train_data_10per))
+#     return train_data_10per
 
 def getLabelsImgs(data):
     labels = []
@@ -66,16 +93,15 @@ def getLabelsImgs(data):
     for img, lb in data:
         labels.append(lb)
         imgs.append(img)
-    return labels, imgs
+    return np.array(labels), np.array(imgs)
 
-def classify(year=2020):
+def classify(year=2015):
 
-    #train_data = getImages()
-    train_data = getImages()
-    train_data_10per = random_sample(train_data)
+    train_data = getImagesTraining(Classification.objects.filter(year__lte=year), year)
+    #train_data_10per = random_sample(train_data)
     # print(train_data)
-    # print(train_data_10per)
-    train_labels, train_images = getLabelsImgs(train_data_10per)
+    #print(train_data_10per)
+    train_labels, train_images = getLabelsImgs(train_data)
     #print(train_labels, train_images)
     print("Training data extracted!")
     # img = getImageFromURL(2020, 75400, 75410)
@@ -91,19 +117,21 @@ def classify(year=2020):
     #     imgs_train.append(cv2.imread((os.path.join(path, f)),1))
 
     imgs_test = []
-    path_test = "./test_data"
-    valid_images = [".jpg", ".png"]
-    for f in os.listdir(path_test):
-        ext = os.path.splitext(f)[1]
-        if ext.lower() not in valid_images:
-            continue
-        imgs_test.append(cv2.resize((cv2.imread((os.path.join(path_test, f)),1)), (32,32)))
+    # path_test = "./test_data"
+    # valid_images = [".jpg", ".png"]
+    # for f in os.listdir(path_test):
+    #     ext = os.path.splitext(f)[1]
+    #     if ext.lower() not in valid_images:
+    #         continue
+    #     imgs_test.append(cv2.resize((cv2.imread((os.path.join(path_test, f)),1)), (32,32)))
 
     # train_images = imgs_train
-    test_images = imgs_test
+    test_data = getImagesTest(year)
+    test_coord, test_images = getLabelsImgs(test_data)
 
     train_images = np.array(train_images)
     test_images = np.array(test_images)
+    #print("TEST", test_images)
     print("Training imgs loaded. Classification starts!")
     m, n, r, k = train_images.shape
     train_imgs_reshaped = train_images.reshape(m, n * r * k)
@@ -137,7 +165,7 @@ def classify(year=2020):
                                                                           models["SVM"],
                                                                           params, train_labels, train_imgs_reshaped)
 
-    hyperparameter_tuning_scores = np.append(hyperparameter_tuning_scores, mean_score, train_labels, train_images)
+    hyperparameter_tuning_scores = np.append(hyperparameter_tuning_scores, mean_score)
     best_estimators = np.append(best_estimators, best_model_estimator)
     # best_scores = np.append(best_scores, best_model_score)
 
@@ -153,21 +181,32 @@ def classify(year=2020):
     pipe = make_pipeline(best_estimators[0], best_estimators[1])
     pipe.fit(train_imgs_reshaped, train_labels)
     prediction = pipe.predict(test_imgs_reshaped)
+    print(best_estimators)
     print(prediction)
 
+    print(test_coord)
+    for i in range(len(prediction)):
+        print(test_coord[i][1])
+        print(test_coord[i][0])
+        Classification.objects.create(tile_id=Tile.objects.get(x_coordinate=test_coord[i][1], y_coordinate=test_coord[i][0]), year=year, label=prediction[i], classified_by="-1")
+        #Classification.objects.filter(tile_id=Tile.objects.get(x_coordinate=test_coord[i][1], y_coordinate=test_coord[i][0]).tid).update(year=year, label=prediction[i], classified_by="-1")
 
 def tune_hyperparams(estimator_name, estimator, estimator_params, train_labels, train_images):
 
-    k_fold = KFold(n_splits=4)
+    k_fold = KFold(n_splits=12)
 
 
     best_model_estimator = Pipeline([("pca", PCA()), (estimator_name, estimator)])
     sum_scores = 0
     best_model_score = 0.0
-    for train_index, test_index in k_fold.split(train_images):
+    # print(len(train_labels))
+    # print(len(train_images))
+    vector = np.vectorize(np.int16)
 
-        Xtrain, Xtest = train_images[train_index], train_images[test_index]
-        Ytrain, Ytest = train_labels[train_index], train_labels[test_index]
+    for train_index, test_index in k_fold.split(train_images):
+        # print(train_index)
+        Xtrain, Xtest = train_images[vector(train_index)], train_images[vector(test_index)]
+        Ytrain, Ytest = train_labels[vector(train_index)], train_labels[vector(test_index)]
 
         pipe = Pipeline([("pca", PCA()), (estimator_name, estimator)])
         search = GridSearchCV(pipe, estimator_params, cv=5, return_train_score=True, n_jobs=-1, verbose=2,
