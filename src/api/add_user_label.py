aff7692173427dbd00f6a08ca7d4ec8f04870ff7
NumberHunter <a.n.karchev@student.tudelft.nl>
2021-05-21 21:28:31 +0200

    add function that allows a user to label a rectangle shaped set of tiles with a single label

---
+++ src/api/add_user_label.py
@@ -0,0 +1,13 @@
+from .models import Tile, Classification
+
+def add_user_label(start_x, start_y, length_x, length_y,year,label,user_id):
+
+    for x in range(start_x, start_x + length_x - 1):
+        for y in range(start_y, start_y + length_y - 1):
+
+            try:
+                Classification.objects.create(tile_id=Tile.objects.get(x_coordinate=x, y_coordinate=y), year=year,
+                                              label=label, classified_by=user_id)
+            except Tile.DoesNotExist:
+                print(x, y)
+                continue
