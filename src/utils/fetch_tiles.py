8bdcb57d65d5e63ad07c3b7571f76bef3d6e9485
Ivan Todorov <i.s.todorov@student.tudelft.nl>
2021-05-24 21:04:25 +0300

    Fixed flake8 issues in src/scripts

--- src/utils/fetch_tiles.py
+++ src/utils/fetch_tiles.py
@@ -2,7 +2,6 @@
 import urllib.request
 
 
-
 def main(year, range_x, range_y, folder):
     """Fetching map tiles"""
 
@@ -11,10 +10,11 @@ def main(year, range_x, range_y, folder):
     for x_coord in range_x:
         for y_coord in range_y:
 
-            res = "https://tiles.arcgis.com/tiles/nSZVuSZjHpEZZbRo/arcgis/rest/services/Historische_tijdreis_" + str(
-                year) + "/MapServer/tile/11/" + str(x_coord) + "/" + str(y_coord)
+            res = "https://tiles.arcgis.com/tiles/nSZVuSZjHpEZZbRo/arcgis/rest/services/Historische_tijdreis_" +\
+                  str(year) + "/MapServer/tile/11/" + str(x_coord) + "/" + str(y_coord)
 
             urllib.request.urlretrieve(res, "../" + folder + "/" + str(y_coord) + "_" + str(x_coord) + ".png")
 
+
 if __name__ == "__main__":
-    main(2020,range(75748,75879),range(75087,75825),"")
+    main(2020, range(75748, 75879), range(75087, 75825), "")
