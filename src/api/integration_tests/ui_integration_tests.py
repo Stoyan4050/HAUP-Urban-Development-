e884af696d1988d784f9e346016203daaa93bbdc
ljongejans <l.j.jongejans@student.tudelft.nl>
2021-05-29 21:12:41 +0200

    Integration testing for change password, register user, log in as guest.

--- src/api/integration_tests/ui_integration_tests.py
+++ src/api/integration_tests/ui_integration_tests.py
@@ -50,31 +50,91 @@ def selection(selector, n=0):
 
 
 email = selection('#id_username')
-email.send_keys('testtest@test.com')
+email.send_keys('hauptest@hauptest.com')
 password = selection('#id_password')
 password.send_keys('!helloT12345')
 map_button = selection('button')
 map_button.click()
 
 url_map = chrome.current_url
-
 if url_map == 'http://127.0.0.1:8000/urban_development/map/':
     print('Correct page')
 else:
     print('Incorrect page')
 
-
 logout_button = selection('#logout-button')
 logout_button.click()
 
 url_after_logout = chrome.current_url
-
 if url_after_logout == 'http://127.0.0.1:8000/urban_development/login/':
     print('Correct page')
 else:
     print('Incorrect page')
 
-assert url_after_logout == 'http://127.0.0.1:8000/urban_development/login/'
+# assert url_after_logout == 'http://127.0.0.1:8000/urban_development/login/'
+
+
+change_password = selection('a', 0)
+change_password.click()
+
+url_change_password = chrome.current_url
+if url_change_password == 'http://127.0.0.1:8000/urban_development/change_password/':
+    print('Correct page')
+else:
+    print('Incorrect page')
+
+change_pwd_email = selection('#id_email')
+change_pwd_email.send_keys('hauptest2@hauptest.com')
+change_pwd_button = selection('button')
+change_pwd_button.click()
+
+resend_password = selection('a', 0)
+resend_password.click()
+
+url_resend_password = chrome.current_url
+if 'http://127.0.0.1:8000/urban_development/send_change_password_email/' in url_resend_password:
+    print('Correct page')
+else:
+    print('Incorrect page')
+
+login_after_change_password = selection('a', 1)
+login_after_change_password.click()
+
+url_after_change_pwd = chrome.current_url
+if url_after_change_pwd == 'http://127.0.0.1:8000/urban_development/login/':
+    print('Correct page')
+else:
+    print('Incorrect page')
+
+register_user = selection('a', 1)
+register_user.click()
+
+url_register_user = chrome.current_url
+if url_register_user == 'http://127.0.0.1:8000/urban_development/register/':
+    print('Correct page')
+else:
+    print('Incorrect page')
+
+return_to_login = selection('a', 0)
+return_to_login.click()
+
+url_login_after_register = chrome.current_url
+if url_login_after_register == 'http://127.0.0.1:8000/urban_development/login/':
+    print('Correct page')
+else:
+    print('Incorrect page')
+
+login_guest = selection('a', 2)
+login_guest.click()
+
+url_map_guest = chrome.current_url
+if url_map_guest == 'http://127.0.0.1:8000/urban_development/map/':
+    print('Correct page')
+else:
+    print('Incorrect page')
+
+user_name = selection('#user-name')
+print(user_name.text)
 
 # map_view_enabled = selection('#map_view_button')
 # print(map_view_enabled.is_enabled())
