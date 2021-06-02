a1a6e1b39aef7e27f38727ce694633efcf28b20e
NumberHunter <a.n.karchev@student.tudelft.nl>
2021-06-02 14:09:32 +0200

    attempt to fix checkstyle again

--- src/scripts/fill_db_classfications.py
+++ src/scripts/fill_db_classfications.py
@@ -4,9 +4,10 @@ fill_db_classfications.py
 
 import os
 import sys
-from api.utils import extract_convert_to_esri
 
 sys.path.insert(0, os.path.abspath('..'))
+
+from api.utils import extract_convert_to_esri
 
 
 def run():
--- src/scripts/fill_db_tiles.py
+++ src/scripts/fill_db_tiles.py
@@ -4,9 +4,10 @@ fill_db_tiles.py
 
 import os
 import sys
-from api.utils import create_tiles
 
 sys.path.insert(0, os.path.abspath('..'))
+
+from api.utils import create_tiles
 
 
 def run():
