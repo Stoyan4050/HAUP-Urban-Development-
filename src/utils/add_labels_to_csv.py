368b9d78a9d6a7035933779f4b577bde8d622438
NumberHunter <a.n.karchev@student.tudelft.nl>
2021-06-07 19:22:08 +0200

    fix some oversights

--- src/utils/add_labels_to_csv.py
+++ src/utils/add_labels_to_csv.py
@@ -17,15 +17,15 @@ def add_label():
     file_name = input()
     data_frame = pandas.read_csv("../data/Wikidata/" + file_name)
 
-    print("Please enter 'True' if the data is of greenery and 'False', otherwise")
-    label = None
-    while label is None:
-        label_name = input()
-
-        if label_name.lower() == "true":
-            label = True
-        elif label_name.lower() == "false":
-            label = False
+    print("Please enter greenery percentage")
+    label = input()
+    # while label is None:
+    #     label_name = input()
+    #
+    #     if label_name.lower() == "true":
+    #         label = True
+    #     elif label_name.lower() == "false":
+    #         label = False
 
     points = data_frame.geo.tolist()
     years = ["Unknown" for _ in range(len(data_frame))]
@@ -33,7 +33,7 @@ def add_label():
     if 'inception' in data_frame.columns:
         years = data_frame.inception.tolist()
 
-    with open("../data/Wikidata/data_1.csv", "a", newline="") as f_object:
+    with open("../data/Wikidata/data.csv", "a", newline="") as f_object:
         writer_object = writer(f_object)
 
         for ind in range(len(points)):
