23a890cf3b37a8996b06d7582561e1df61d1dc07
NumberHunter <a.n.karchev@student.tudelft.nl>
2021-06-17 20:52:25 +0200

    put some utils functions into their own files

--- src/api/utils/send_email.py
+++ src/api/utils/send_email.py
@@ -2,8 +2,7 @@
 utils.py
 """
 
-import requests
-from bs4 import BeautifulSoup
+
 from django.core.exceptions import ObjectDoesNotExist
 from django.core.mail import EmailMessage
 from django.template.loader import render_to_string
@@ -11,31 +10,6 @@ from django.utils.encoding import force_bytes
 from django.utils.http import urlsafe_base64_encode
 from ..models import User
 from ..tokens import TOKEN_GENERATOR
-
-
-def extract_available_years():
-    """
-    def extract_available_years()
-    """
-
-    page = requests.get("https://tiles.arcgis.com/tiles/nSZVuSZjHpEZZbRo/arcgis/rest/services")
-    soup = BeautifulSoup(page.content, 'html.parser')
-    years = {}
-
-    for hyperlink in soup.select("ol[id=serviceList] > li > a[id=l1]"):
-        if hyperlink.text.startswith("Historische_tijdreis_"):
-            reference = hyperlink.text[len("Historische_tijdreis_"):]
-
-            if "_" in reference:
-                for i in range(int(reference[: reference.index("_")]), int(reference[reference.index("_") + 1:]) + 1):
-                    years[i] = reference
-            else:
-                years[int(reference)] = reference
-
-    if 2020 not in years:
-        years[2020] = "2020"
-
-    return years
 
 
 def send_email(uid, domain, email_subject, email_template):
