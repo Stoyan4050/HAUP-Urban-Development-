00577940717a9205f989816b01720bafdb6ad628
Ivan Todorov <i.s.todorov@student.tudelft.nl>
2021-06-24 18:57:46 +0300

    Added tests for the login_form and the login_view

--- src/api/tests/__init__.py
+++ src/api/tests/__init__.py
@@ -3,4 +3,6 @@ __init__.py
 """
 
 from api.tests import test_color_detection,\
-                      test_get_image_from_url
+                      test_get_image_from_url,\
+                      test_login_form,\
+                      test_login_view
